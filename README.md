3722c0075c467fb9b8387b50983e0b491f9b662d
Jen <111.000@0112358.io>
2021-12-11 13:36:31 -0800

    metamask wip

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ A decentralized, p2p light drawing platform that shares across connected network
 
 ## Intended Goals
 
-1. To create an open social platform such that authentication is seamless and/or automated (no usernames, passwords, etc.)
+1. To create an open social platform such that authentication is seamless and/or automated (no usernames, passwords, etc.)*
 2. Limit to canvas drawing and remove text input to both simplify input requirements (no categories, dropdowns, additional fields, etc.) and increase creative flexibility.
 3. Allow for the highest number of potential participants (no initial gatekeeping) with the ability to mute posts from specified users locally (affects personal preferences, is not exposed outside to the network).
-4. Has the least amount of dependencies, third party libraries and code (could be less but this is the first attempt!)
-5. Has to maintain a basic level of security for users (send a PR if you see anything that could be improved)
+4. Contains the least amount of dependencies, third party libraries and code (could be less but this is the first attempt!)
+5. Maintain a basic level of security for users - this isn't meant to protect sensitive data - this is intended for avoiding impersonation attempts (send a PR if you see anything that could be improved)
+  
+\* Depending on the branch, some contain working examples while some aren't ready yet. The default one on `main` assumes a user has a Metamask wallet (working). The `tweetnacl` one is still under development/testing.
 
 This uses [DAM - Daisy-chain Ad-hoc Mesh-network](https://gun.eco/docs/DAM) - read about it for more details.
--- collab.js
+++ collab.js
@@ -12,83 +12,65 @@
   let opacity = 1.0
   let outgoingActive = true
   const gun = Gun(location.host ? location.origin + '/gun' : 'http://localhost:8080/gun')
-  const newNonce = () => randomBytes(nacl.box.nonceLength)
-  const generateKeyPair = () => nacl.box.keyPair()
 
   let timeline = gun.get('timeline')
   let ol = document.createElement('ol')
-  let mutedSaved = localStorage.getItem('muted')
+  let mutedLocal = localStorage.getItem('muted')
   incoming.appendChild(ol)
-
-  let keypair = localStorage.getItem('keypair')
-
-  function encrypt(secretOrSharedKey, json, key) {
-    const nonce = newNonce()
-    const messageUint8 = aotb(JSON.stringify(json))
-    const encrypted = key ? nacl.box(messageUint8, nonce, key, secretOrSharedKey) : nacl.box.after(messageUint8, nonce, secretOrSharedKey)
-    const fullMessage = new Uint8Array(nonce.length + encrypted.length)
-    fullMessage.set(nonce)
-    fullMessage.set(encrypted, nonce.length)
-    const base64FullMessage = encodeBase64(fullMessage)
-    return base64FullMessage
-  }
-  
-  function decrypt(secretOrSharedKey, messageWithNonce, key) {
-    const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce)
-    const nonce = messageWithNonceAsUint8Array.slice(0, box.nonceLength)
-    const message = messageWithNonceAsUint8Array.slice(box.nonceLength, messageWithNonce.length)
-    const decrypted = key ? box.open(message, nonce, key, secretOrSharedKey) : box.open.after(message, nonce, secretOrSharedKey)
-  
-    if (!decrypted) {
-      throw new Error('Could not decrypt message')
-    }
-  
-    return JSON.parse(encodeUTF8(decrypted))
-  }
 
   sigPad.on()
 
-  if (muted) {
-    mutedSaved = JSON.parse(mutedSaved)
-  } else {
-    mutedSaved = {}
-  }
-
-  function mute(publicKey) {
-    mutedSaved[publicKey] = Date.now()
-    localStorage.setItem('muted', JSON.stringify(muted))
-  }
-
-  function unmute(publicKey) {
-    delete mutedSaved[publicKey]
-    localStorage.setItem('muted', JSON.stringify(muted))
-  }
-
-  if (!keypair || !keypair.publicKey) {
-    const newNonce = () => randomBytes(box.nonceLength)
-    keypair = generateKeyPair()
-    console.log(keypair)
-    localStorage.setItem('keypair', JSON.stringify(keypair))
-    let users = gun.get('users')
-    const user = {
-      publicKey: keypair.publicKey,
-      url: null
-    }
-    users.set(user)
-  }
-
-  gun.get('users').get(keypair.publicKey).once((_, key) => {
-    console.log('>>>>>>>>', keypair, key)
+  let id = localStorage.getItem('userId')
+ 
+  gun.get('users').get(id).once((_, key) => {
+    console.log('>>>>>>>>', id, key)
   })
 
+  if (mutedLocal) {
+    mutedLocal = JSON.parse(mutedLocal)
+  } else {
+    mutedLocal = {}
+  }
+
+  function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+  }
+
+  function mute(userId) {
+    mutedLocal[userId] = Date.now()
+    localStorage.setItem('muted', JSON.stringify(mutedLocal))
+  }
+
+  function unmute(userId) {
+    delete mutedLocal[userId]
+    localStorage.setItem('muted', JSON.stringify(mutedLocal))
+  }
+
+  const provider = window.ethereum ? new ethers.providers.Web3Provider(window.ethereum) : null
+  const signer = provider ? provider.getSigner() : null
+  console.log()
+  const getAddress = async () => {
+    if (provider) {
+      await provider.send('eth_requestAccounts', [])
+      id = await signer.getAddress()
+      console.log("Your wallet is " + id) 
+    } else {
+      console.log('no provider')
+    }
+  }
+
+  getAddress
+
   let setUrl = setInterval(() => {
-    let currentUser = gun.get('users').get(keypair.publicKey)
+    let currentUser = gun.get('users').get(id)
     const url = localStorage.getItem('url')
 
     if (currentUser.url !== url) {
       console.log('updating url ==> ', url)
-      gun.get('users').get(keypair.publicKey).put({
-        publicKey: keypair.publicKey,
+      gun.get('users').get(id).put({
+        id: id,
         url: url
       })
       clearInterval(setUrl)
@@ -135,21 +117,16 @@
     }
   }
 
-  window.onkeyup = (ev) => {
-    let data = sigPad.toData()
-    if (ev.keyCode === 13 && data) {
-      const messageKeyPair = () => nacl.box.keyPair()
+  window.onkeyup = function (ev) {
+    if (ev.keyCode === 13) {
       const created = Date.now()
       const content = {
-        id: messageKeyPair.publicKey,
+        id: uuidv4(),
         created: created,
-        publicKey: keypair.publicKey,
-        messageKey: messageKeyPair.privateKey,
-        png: encrypt(nacl.box.before(messageKeyPair.publicKey, keypair.secretKey), sigPad.toDataURL())
+        userId: id,
+        png: sigPad.toDataURL()
       }
-
-      gun.get('timeline').set(content)
-      get.get('users').get(keypair.publicKey).get('posts').set(content)
+      timeline.set(content)
       sigPad.clear()
     }
   }
@@ -162,33 +139,26 @@
     }
   })
 
-  timeline.map().on(ig => {
+  timeline.map().on((ig) => {
     if (!document.getElementById('i-' + ig.id)) {
-      try {
-        const decrypted = decrypt(nacl.box.before({ 
-          publicKey: ig.publicKey, 
-          secretKey: ig.secretKey
-        }, keypair.secretKey), ig.png)
-        let li = document.createElement('li')
-        let span = document.createElement('span')
-        span.textContent = ig.publicKey
-        span.addEventListener('click', (ev) => {
-          ev.preventDefault()
-          let muteBtn = document.createElement('button')
-          muteBtn.className = 'mute'
-          muteBtn.onclick = (ev) => {
-            ev.preventDefault()
-            
-          }
-        })
-        let img = new Image()
-        img.id = 'i-' + ig.publicKey
-        img.addEventListener('load', () => { })
-        img.src = decrypted
-        li.appendChild(img)
-        li.appendChild(span)
-        ol.insertBefore(li, ol.firstChild)
-      } catch(e) {}
+      let li = document.createElement('li')
+      let span = document.createElement('span')
+      span.textContent = ig.userId
+      span.addEventListener('click', (ev) => {
+        ev.preventDefault()
+        let muteBtn = document.createElement('button')
+        muteBtn.className = 'mute'
+        muteBtn.onclick = function () {
+          
+        }
+      })
+      let img = new Image()
+      img.id = 'i-' + ig.id
+      img.addEventListener('load', () => { })
+      img.src = ig.png
+      li.appendChild(img)
+      li.appendChild(span)
+      ol.insertBefore(li, ol.firstChild)
     }
   })
 
